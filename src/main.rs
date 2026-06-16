5c2c09fc02e4757ff98c9958776ea0675ab2883f
Arthur Lamidel <arthurlamidel@gmail.com>
2026-06-16 09:17:29 +0200

    Setup clap

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,31 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// List all available levels
+    List,
+    /// Generate the graph of a level
+    Graph { level: String },
+}
+
 fn main() {
+    use Commands as C;
+
+    let cli = Cli::parse();
+
+    match &cli.command {
+        C::Graph { level } => generate_graph(level),
+        C::List => todo!(),
+    }
+}
+
+fn generate_graph(level: &str) {
+    println!("{level}");
 }
